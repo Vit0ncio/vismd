0b130b0f3441c1b30b3e5bf2e77916f5d2d10fe5
Vit0ncio <vitorlgv@proton.me>
2026-02-04 09:35:03 -0300

    Minor refatoring in heading function

--- src/main/Bold.java
+++ src/main/Bold.java
@@ -1,6 +1,6 @@
 public class Bold {
   public String contain_bold(String text) {
-    if (text.startsWith("**"))
+    if (text.startsWith("**") || text.startsWith("# "))
       return "\033[1m" + text;
     else
       return null;
--- src/main/Heading.java
+++ src/main/Heading.java
@@ -1,7 +1,9 @@
 public class Heading {
+  Bold bold = new Bold();
+
   public String which_heading(String text) {
     if (text.startsWith("# "))
-      return "\033[4m" + "\033[1m" + text;
+      return "\033[4m" + text.toUpperCase() + bold.contain_bold(text);
     else if (text.startsWith("## "))
       return "\033[4m" + text;
     else if (text.startsWith("### "))
